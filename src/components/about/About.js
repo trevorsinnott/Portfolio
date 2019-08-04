f87091aad35215ea8dd91f993309ffe3d3ff010d
Trevor <sinnott.trevor@gmail.com>
2019-08-04 14:38:43 +0900

    update About component including adding avatar images

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -1,10 +1,14 @@
 import React from "react";
-import { Typography, Container, makeStyles } from "@material-ui/core";
+import { Typography, makeStyles, Avatar, Grid } from "@material-ui/core";
 import { loadCSS } from "fg-loadcss";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles(() => ({
   about: {
-    height: "100vh"
+    height: "120vh"
+  },
+  avatar: {
+    height: 100,
+    width: 100
   }
 }));
 
@@ -19,16 +23,37 @@ function About() {
   }, []);
 
   return (
-    <Container className={classes.about}>
-      <Typography variant="h2">About</Typography>
-      <Typography variant="body1" paragraph>
-        Hi! I'm Trevor, a front-end developer who loves learning and building
-        new things. Whether that's making PWA's with React, going to developer
-        meetups, or writing command line games in Ruby. I love surrounding
-        myself with people who are passionate and curious about what they do. If
-        that means you, please get in touch!
-      </Typography>
-    </Container>
+    <Grid
+      className={classes.about}
+      container
+      spacing={3}
+      direction="column"
+      alignItems="center"
+      justify="space-evenly"
+    >
+      <Grid
+        container
+        item
+        lg={4}
+        md={4}
+        sm={4}
+        justify="center"
+        alignItems="center"
+      >
+        <Avatar className={classes.avatar} src="./avatar.jpg">
+          TS
+        </Avatar>
+      </Grid>
+      <Grid item lg={8} md={8} sm={8}>
+        <Typography className={classes.content} variant="body1">
+          Hi! I'm Trevor, and I'm excited about learning and building new
+          things. Whether that's making PWA's with React, going to developer
+          meetups, or writing command line games in Ruby. I love surrounding
+          myself with people who are passionate and curious about what they do.
+          If that means you, please get in touch!
+        </Typography>
+      </Grid>
+    </Grid>
   );
 }
 
