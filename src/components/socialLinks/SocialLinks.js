93fe0bf73563f7db0fbabc9182dba2e6a8d7bb27
Trevor <sinnott.trevor@gmail.com>
2019-08-16 12:20:59 +0900

    update email address in email link

--- src/components/socialLinks/SocialLinks.js
+++ src/components/socialLinks/SocialLinks.js
@@ -29,7 +29,7 @@ function SocialLinks() {
         </SvgIcon>
         Linkedin
       </Link>
-      <Link href="mailto: sinnott.trevor@gmail.com">
+      <Link href="mailto: trevor@trevorsinnott.com">
         <SvgIcon viewBox="0 0 24 24">
           <path d="M20,8L12,13L4,8V6L12,11L20,6M20,4H4C2.89,4 2,4.89 2,6V18A2,2 0 0,0 4,20H20A2,2 0 0,0 22,18V6C22,4.89 21.1,4 20,4Z" />
         </SvgIcon>
